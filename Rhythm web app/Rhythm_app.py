64a220c6b9dd3db7c12323e4b932e943415542b7
Andrudewt <sixnineeighttwotwozeroone@yahoo.com>
2024-03-26 00:02:41 +0400

    .

--- Rhythm web app/Rhythm_app.py
+++ Rhythm web app/Rhythm_app.py
@@ -13,9 +13,12 @@ def get_pic_array(path):
             pictures.append(cv2.imread(os.path.join(path, filename), cv2.IMREAD_GRAYSCALE))
     return pictures
 
-def generate_phrase(pictures, nuber):
+def generate_phrase(pictures, nuber, name):
     """ horizontal image concatenation  """
-    return cv2.hconcat(random.sample(pictures, (int(nuber))))
+    result = cv2.hconcat(random.sample(pictures, (int(nuber))))
+    cv2.imwrite(img_path + f"result/{name}.png", result)
+
+
 
 app = Flask(__name__)
 
@@ -26,25 +29,20 @@ def start():
         number = request.form["button"]
         if int(number) < 3:
             pictures = get_pic_array(img_path)
-            result = generate_phrase(pictures, number)
-            cv2.imwrite(img_path + "result/result.png", result)
+            generate_phrase(pictures, number, "result")
             return render_template("start.html", the_title="Here your rhythm",
                                    flag=True)
         else:
             if int(number) == 3:
                 pictures = get_pic_array(img_path)
-                result = generate_phrase(pictures, int(number)-1)
-                result_2 = generate_phrase(pictures, int(number)-2)
-                cv2.imwrite(img_path + "result/result.png", result)
-                cv2.imwrite(img_path + "result/result_2.png", result_2)
+                generate_phrase(pictures, int(number)-1, "result")
+                generate_phrase(pictures, int(number)-2, "result_2")
                 return render_template("start.html", the_title="Here your rhythm",
                                        flag=True, flag_3=True)
             else:
                 pictures = get_pic_array(img_path)
-                result = generate_phrase(pictures, int(number) - 2)
-                result_2 = generate_phrase(pictures, int(number) - 2)
-                cv2.imwrite(img_path + "result/result.png", result)
-                cv2.imwrite(img_path + "result/result_2.png", result_2)
+                generate_phrase(pictures, int(number) - 2, "result")
+                generate_phrase(pictures, int(number) - 2, "result_2")
                 return render_template("start.html", the_title="Here your rhythm",
                                        flag=True, flag_3=True)
     else:
