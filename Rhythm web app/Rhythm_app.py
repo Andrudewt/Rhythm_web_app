7b18dd4c4654c5c125cb118cff4ba71393ba854e
Andrudewt <sixnineeighttwotwozeroone@yahoo.com>
2024-03-26 23:02:31 +0400

    .

--- Rhythm web app/Rhythm_app.py
+++ Rhythm web app/Rhythm_app.py
@@ -3,7 +3,6 @@ import cv2
 import os
 import random
 
-img_path = r"static/images/"
 
 def get_pic_array(path):
     """ collects the pictures from the folder into an array """
@@ -14,36 +13,34 @@ def get_pic_array(path):
     return pictures
 
 def generate_phrase(pictures, number, name):
-    """ horizontal image concatenation  """
-
+    """ horizontal image concatenation randomly
+    chosen according to desired quantity """
+    result = cv2.hconcat(random.sample(pictures, number))
     if number % 2:
-        pass
-    else:
-        result = cv2.hconcat(random.sample(pictures, number))
-        cv2.imwrite(img_path + f"result/{name}.png", result)
+        result = cv2.hconcat((result, extend_pic))
+    cv2.imwrite(img_path + f"result/{name}.png", result)
 
-
+img_path = r"static/images/"
+extend_pic = cv2.imread(r"static/images/result/extend.png", cv2.IMREAD_GRAYSCALE)
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def start():
-    if request.method == "POST" and request.form["button"] in "123456789" :
+    pictures = get_pic_array(img_path)
+    if request.method == "POST" and request.form["button"] in "123456789":
         number = int(request.form["button"])
         if number < 3:
-            pictures = get_pic_array(img_path)
             generate_phrase(pictures, number, "result")
             return render_template("start.html", the_title="Here your rhythm",
                                    flag=True)
         else:
             if int(number) == 3:
-                pictures = get_pic_array(img_path)
                 generate_phrase(pictures, int(number)-1, "result")
                 generate_phrase(pictures, int(number)-2, "result_2")
                 return render_template("start.html", the_title="Here your rhythm",
                                        flag=True, flag_3=True)
             else:
-                pictures = get_pic_array(img_path)
                 generate_phrase(pictures, int(number) - 2, "result")
                 generate_phrase(pictures, int(number) - 2, "result_2")
                 return render_template("start.html", the_title="Here your rhythm",
